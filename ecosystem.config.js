8c23ac94e3900524e487045653d1235880252eef
Alaister Young <a@alaisteryoung.com>
2024-06-27 21:29:12 +0800

    chore: Update npm start script to include port variable

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'website',
-      script: 'npm run start',
+      script: 'npm run start -- --port $PORT',
       instances: 4,
       exec_mode: 'cluster',
       watch: true,
       increment_var: 'PORT',
       env: {
         PORT: 4000,
-        NEXT_PUBLIC_BASE_PATH: '/next',
       },
     },
   ],
